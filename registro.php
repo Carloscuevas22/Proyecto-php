b217b1d60c904799dc12aba6bd22cf8d0ec99390
Ricardo <pittiricardo7@gmail.com>
2021-05-12 00:07:54 -0500

    final git

--- registro.php
+++ registro.php
@@ -1,10 +1,10 @@
 ﻿<?php
-$db_host="localhost";
+$servername="localhost";
 $db_user="root";
 $db_password="";
 $db_name="suscribir";
 $db_table_name="usuarios";
-   $db_connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+   $db_connection = mysqli_connect($servername, $db_user, $db_password, $db_name);
 
 if (!$db_connection) {
 	die('No se ha podido conectar a la base de datos');
@@ -12,6 +12,9 @@ if (!$db_connection) {
 $subs_name = utf8_decode($_POST['nombre']);
 $subs_last = utf8_decode($_POST['apellido']);
 $subs_email = utf8_decode($_POST['email']);
+$subs_phone =utf8_decode($_POST['telefono']);
+$subs_edad =utf8_decode($_POST['edad']);
+$subs_agree =utf8_decode($_POST['accede']);
 
 $resultado=mysqli_query($db_connection,"SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'");
 
@@ -23,13 +26,13 @@ if (mysqli_num_rows($resultado)>0)
 
 } else {
 	
-	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`Nombre` , `Apellido` , `Email`) VALUES ("' . $subs_name . '", "' . $subs_last . '", "' . $subs_email . '")';
+	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`Nombre` , `Apellido` , `Email`,`Telefono`,`Edad`,`Accede`) VALUES ("' . $subs_name . '", "' . $subs_last . '", "' . $subs_email . '", "' . $subs_phone . '", "' . $subs_edad . '","' . $subs_agree . '")';
 
    mysqli_select_db($db_connection, $db_name);
    $retry_value = mysqli_query($db_connection, $insert_value);
 
    if (!$retry_value) {
-      die('Error: ' . mysqli_error());
+      die('Error: ' . mysqli_error($db_connection));
    }
 	
    mysqli_free_result($resultado);
